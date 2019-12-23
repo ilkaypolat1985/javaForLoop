5494c7e12c254577f476f137ce0cb9c013278b76
ilkaypolat1985 <ilkaypolat1985@gmail.com>
2019-12-23 15:35:52 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("******************************");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        System.out.println("******************************");
+        for (int i = 1; i < 10; i += 2) {
+            System.out.println(i);
+        }
+
+        System.out.println();
+        System.out.println("******************************");
+        for (int i = 2; i < 10; i += 2) {
+            System.out.println(i);
+        }
+
+    }
+}
